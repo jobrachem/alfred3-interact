71a8d34af1cfe147af80ae124684fc9ac5c09f77
Johannes Brachem <jobrachem@posteo.de>
2021-05-27 18:41:03 +0200

    Revert "Revert "rewrite match_groupwise""
    This reverts commit da3db898933e309ca94e102b493bf10074b9523b.

--- src/alfred3_interact/match.py
+++ src/alfred3_interact/match.py
@@ -24,7 +24,7 @@ from ._util import saving_method
 from ._util import MatchingError
 from ._util import BusyGroup
 from ._util import MatchingTimeout
-
+from ._util import NoMatch
 
 class MatchMakerIO:
     def __init__(self, matchmaker):
@@ -455,10 +455,7 @@ class MatchMaker:
 
     def match_groupwise(
         self,
-        match_timeout: int = 60 * 30,
-        ping_timeout: int = 15,
-        timeout_page=None,
-        raise_exception: bool = False,
+        ping_timeout: int = 15
     ) -> Group:
         """
         Waits until there are enough participants for a full group, then
@@ -538,15 +535,31 @@ class MatchMaker:
         """
         if not self._check_activation():
             return
-        
 
         if not saving_method(self.exp) == "mongo":
             raise MatchingError("Must use a database for groupwise matching.")
 
+        if self.member and self.group:
+            self.log.info(f"match_groupwise was called, but {self.member} was already matched to \
+                {self.group}. Returning group.")
+            return self.group
+
         self._set_ping_timeout(ping_timeout)
 
-        self.member = GroupMember(self)
-        self.member._save()
+        self.member = self._init_member()
+        
+        self.group = self._do_match_groupwise(ping_timeout=ping_timeout)
+
+        if not self.group:
+            raise NoMatch
+
+        
+        self.log.info(f"{self.group} filled in groupwise match.")
+        self._save_infos()
+        return self.group
+
+
+
         start = time.time()
 
         i = 0
@@ -579,6 +592,17 @@ class MatchMaker:
             self.log.info(f"{self.group} filled in groupwise match.")
             self._save_infos()
             return self.group
+    
+    def _init_member(self):
+        if self.member:
+            return self.member
+        
+        member = GroupMember(self)
+        member._save()
+        if member.expired:
+            raise SessionTimeout
+        else:
+            return member
 
     def toggle_activation(self) -> str:
         """
