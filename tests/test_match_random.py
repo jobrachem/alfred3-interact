fa7d63c51b0ba46750e2be757e6faeddb106fb7c
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2022-01-12 16:35:18 +0100

    apply formatting

--- tests/test_match_random.py
+++ tests/test_match_random.py
@@ -4,6 +4,7 @@ import random
 from alfred3_interact import SequentialSpec, MatchMaker, NoMatch
 from alfred3_interact.spec import ParallelSpec
 
+
 def test_clear(exp):
     """
     Just for clearing the database in case a test breaks down with an error.
@@ -12,7 +13,6 @@ def test_clear(exp):
 
 
 class TestSequential:
-
     def test_match_random(self, exp):
         spec1 = SequentialSpec("a", "b", nslots=5, name="test1")
         spec2 = SequentialSpec("a2", "b2", nslots=5, name="test2")
@@ -23,7 +23,7 @@ class TestSequential:
         group = mm.match_random()
 
         assert group.data.spec_name == "test1"
-    
+
     def test_match_random2(self, exp):
         spec1 = SequentialSpec("a", "b", nslots=5, name="test1")
         spec2 = SequentialSpec("a2", "b2", nslots=5, name="test2")
@@ -34,7 +34,7 @@ class TestSequential:
         group = mm.match_random()
 
         assert group.data.spec_name == "test2"
-    
+
     def test_fill_group(self, exp_factory):
         exp1 = exp_factory("__exp1")
         exp2 = exp_factory("__exp2")
@@ -50,7 +50,7 @@ class TestSequential:
         group2 = mm2.match_random()
 
         assert group1 == group2
-    
+
     def test_match_random_local(self, lexp):
         spec1 = SequentialSpec("a", "b", nslots=5, name="test1")
         spec2 = SequentialSpec("a2", "b2", nslots=5, name="test2")
@@ -61,7 +61,7 @@ class TestSequential:
         group = mm.match_random()
 
         assert group.data.spec_name == "test2"
-    
+
     def test_match_random2_local(self, lexp):
         spec1 = SequentialSpec("a", "b", nslots=5, name="test1")
         spec2 = SequentialSpec("a2", "b2", nslots=5, name="test2")
@@ -72,7 +72,7 @@ class TestSequential:
         group = mm.match_random()
 
         assert group.data.spec_name == "test1"
-    
+
     def test_minwait_fail(self, exp):
         spec1 = SequentialSpec("a", "b", nslots=5, name="test1")
         spec2 = SequentialSpec("a2", "b2", nslots=5, name="test2")
@@ -82,7 +82,7 @@ class TestSequential:
         random.seed(123)
         with pytest.raises(NoMatch):
             mm.match_random(wait=10)
-    
+
     def test_minwait_success(self, exp):
         spec1 = SequentialSpec("a", "b", nslots=5, name="test1")
         spec2 = SequentialSpec("a2", "b2", nslots=5, name="test2")
@@ -92,14 +92,14 @@ class TestSequential:
         random.seed(123)
         with pytest.raises(NoMatch):
             mm.match_random(wait=2)
-        
+
         time.sleep(2)
 
         random.seed(123)
         group1 = mm.match_random(wait=2)
 
         assert group1.data.spec_name == "test2"
-    
+
     def test_nmin_fail(self, exp):
         spec1 = SequentialSpec("a", "b", nslots=5, name="test1")
         spec2 = SequentialSpec("a2", "b2", nslots=5, name="test2")
@@ -108,11 +108,10 @@ class TestSequential:
 
         random.seed(123)
         with pytest.raises(NoMatch):
-            mm.match_random(wait= 10, nmin=2)
-    
-    
+            mm.match_random(wait=10, nmin=2)
+
+
 class TestParallel:
-    
     def test_nmin_success(self, exp_factory):
         exp1 = exp_factory()
         exp2 = exp_factory()
@@ -125,13 +124,48 @@ class TestParallel:
         random.seed(123)
         with pytest.raises(NoMatch):
             mm1.match_random(wait=10, nmin=2)
-        
+
         group2 = mm2.match_random(wait=10, nmin=2)
-        
+
         random.seed(123)
         group1 = mm1.match_random(wait=10, nmin=2)
         assert group1.data.spec_name == "test"
         assert group2.data.spec_name == "test"
+
+    def test_nmin_second(self, exp_factory):
+        exp1 = exp_factory()
+        exp2 = exp_factory()
+        exp3 = exp_factory()
+        exp4 = exp_factory()
+
+        spec = ParallelSpec("a", "b", nslots=5, name="test")
+
+        mm1 = MatchMaker(spec, exp=exp1)
+        mm2 = MatchMaker(spec, exp=exp2)
+        mm3 = MatchMaker(spec, exp=exp3)
+        mm4 = MatchMaker(spec, exp=exp4)
+
+        random.seed(123)
+        with pytest.raises(NoMatch):
+            mm1.match_random(wait=2, nmin=4)
+
+        with pytest.raises(NoMatch):
+            mm2.match_random(wait=2, nmin=4)
+
+        with pytest.raises(NoMatch):
+            mm3.match_random(wait=2, nmin=4)
+
+        group4 = mm4.match_random(wait=2, nmin=4)
+
+        random.seed(123)
+        group1 = mm1.match_random(nmin=4)
+        group2 = mm2.match_random(nmin=4)
+        group3 = mm3.match_random(nmin=4)
+
+        assert group1.data.spec_name == "test"
+        assert group2.data.spec_name == "test"
+        assert group3.data.spec_name == "test"
+        assert group4.data.spec_name == "test"
 
     def test_three(self, exp_factory):
         exp1 = exp_factory()
@@ -192,4 +226,3 @@ class TestParallel:
         assert group3.me.role == next(group3.roles.roles_of([exp3.session_id]))
 
         assert len(set([group1.me.role, group2.me.role, group3.me.role])) == 3
-
